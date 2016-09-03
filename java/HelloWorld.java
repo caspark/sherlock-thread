782be1ee398691d015bb42e509400401ad520422
Caspar Krieger <git@asparck.com>
2016-09-03 19:19:17 +1000

    Improve sample Java app

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,10 +1,16 @@
 public class HelloWorld {
     public static void main(String[] args) {
         new Thread(() -> {
-            System.out.println("OJava thread println on STDOUT");
+            System.out.println("Java thread1 l1 STDOUT\nJava thread1 l2 STDOUT");
+            System.out.println("Java thread1 l3 STDOUT");
         }).start();
 
-        System.out.println("OJava println on STDOUT\nLine 2 on STDOUT");
-        System.err.println("EJava println on STDERR\nLine 2 on STDERR");
+        new Thread(() -> {
+            System.out.println("Java thread2 l1 STDOUT\nJava thread2 l2 STDOUT");
+            System.out.println("Java thread2 l3 STDOUT");
+        }).start();
+
+        System.out.println("Java main l1 STDOUT\nJava main l2 STDOUT");
+        System.err.println("Java main l1 STDERR\nJava main l2 STDERR");
     }
 }
